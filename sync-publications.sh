53ec682033242b6f2e1fd94dda825aabbbcce2b9
Kartik Singhal <kartiksinghal@gmail.com>
2022-05-21 07:05:09 -0500

    Add publications sync script

---
+++ sync-publications.sh
@@ -0,0 +1,12 @@
+# Mini README:
+#   Make sure Hugo Academic CLI is installed on your local environment
+#   https://github.com/wowchemy/hugo-academic-cli
+#   1. To add new publications, add an entry to publications.bib
+#      Consider including the abstract too.
+#   2. Launch this script ./sync-publications.sh
+
+academic import --bibtex publications.bib
+echo
+echo "Be very careful while using the --overwrite option, it will overwrite the"
+echo "carefully curated publication types. It is recommended to edit the generated"
+echo "index.md file(s) directly for small changes to how the publication is shown."
