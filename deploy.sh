90445e2038ac2e6a70c741e212428e9633ec7320
Kartik Singhal <ks@cs.uchicago.edu>
2022-05-21 18:45:11 -0500

    fix permissions for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 TARGET='/stage/web_static/chiqp/htdocs/'
-hugo -d $TARGET && cd $TARGET && find . -type f -exec chmod 644 {} + && find ./ -type d -exec chmod 755 {} + && cd - || exit
+hugo -d $TARGET && cd $TARGET && find . -type f -exec chmod 664 {} + && find ./ -type d -exec chmod 775 {} + && cd - || exit
