0bbd34a1771c0ff15ac9dec1b06484ae7cab5076
Kartik Singhal <ks@cs.uchicago.edu>
2022-06-13 10:01:22 -0500

    Update deploy script to use rsyncwq

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 TARGET='/stage/web_static/chiqp/htdocs/'
-hugo -d $TARGET && cd $TARGET && find . -type f -exec chmod 664 {} + && find ./ -type d -exec chmod 775 {} + && cd - || exit
+hugo
+rsync -rltoDOz --delete public/ $TARGET
